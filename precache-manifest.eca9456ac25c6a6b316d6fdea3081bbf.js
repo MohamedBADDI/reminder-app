d72200041001b970b40bcb0bb87372f575a2f6e5
MohamedBADDI <baddisimo@gmail.com>
2020-04-18 17:07:04 +0100

    Updates

---
+++ precache-manifest.eca9456ac25c6a6b316d6fdea3081bbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329ab610c46b6cad3dea07b1ea0f8b7b",
+    "url": "/reminder-app/index.html"
+  },
+  {
+    "revision": "c2be58c925e11249f708",
+    "url": "/reminder-app/static/css/2.9e7b09f4.chunk.css"
+  },
+  {
+    "revision": "449062209e8c6a57c4b6",
+    "url": "/reminder-app/static/css/main.094d71d0.chunk.css"
+  },
+  {
+    "revision": "c2be58c925e11249f708",
+    "url": "/reminder-app/static/js/2.ecf82c9f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reminder-app/static/js/2.ecf82c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449062209e8c6a57c4b6",
+    "url": "/reminder-app/static/js/main.f165225d.chunk.js"
+  },
+  {
+    "revision": "e8859931ad5f2919fb51",
+    "url": "/reminder-app/static/js/runtime-main.8bd60172.js"
+  }
+]);
